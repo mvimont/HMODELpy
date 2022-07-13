cef781011648bcdd845089e455e90126955fc263
michael <michael@mikethinkpad.bermonthome>
2022-07-12 22:08:42 -0400

    add docstingto simulation object

--- simulation.py
+++ simulation.py
@@ -1,6 +1,7 @@
 from setting import Grid
 
 class Simulation:
+    """General simulation object"""
     def __init__(self, x_axis_len : int, y_axis_len : int, years_bp : int):
         self.x_axis_len = x_axis_len
         self.y_axis_len = y_axis_len
